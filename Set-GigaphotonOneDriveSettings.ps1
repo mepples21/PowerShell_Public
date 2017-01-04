ee1d2c19bf84f7802be12f01855d40805fa8d888
mepples21 <michael@rvrwest.com>
2017-01-04 15:39:29 -0600

    File Created
    Created new file

---
+++ Set-GigaphotonOneDriveSettings.ps1
@@ -0,0 +1,43 @@
+Request-SPOPersonalSite -UserEmails "lane.c@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "zane.d@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "khem.r@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "joe.g@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "james.g@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "tomas.g@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "alex.h@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "tom.h@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "levi.j@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "nathan.k@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "jeff.k@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "marco.m@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "pedro.n@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "john.p@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "colton.r@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "mike.s@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "isaac.s@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "dennis.s@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "brady.s@gigaphotonusa.com" -NoWait
+Request-SPOPersonalSite -UserEmails "yusuke.y@gigaphotonusa.com" -NoWait
+
+Start-Sleep -Seconds 120
+
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/lane_c_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/zane_d_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/khem_r_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/joe_g_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/james_g_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/tomas_g_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/alex_h_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/tom_h_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/levi_j_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/nathan_k_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/jeff_k_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/marco_m_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/pedro_n_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/john_p_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/colton_r_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/mike_s_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/isaac_s_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/dennis_s_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/brady_s_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
+Set-SPOSite -Identity https://komatsuna-my_sharepoint_com/personal/yusuke_y_gigaphotonusa_com -StorageQuota 51200 -StorageQuotaWarningLevel 46080
